267412a9aafd3762a64fa144483ba5d7198d9d0a
Bastian Awischus <bastian.awischus@zii.aero>
2020-09-29 17:29:16 -0700

    added unzip function, added dummy file data

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-# download from team city to buildSourceBinaryFiles
-# Unzip the files to local dir
-# Do folder manipulation
+# 1. download from team city to buildSourceBinaryFiles (done manually for now)
+# 2. Unzip the files to local dir
+# 3. Do folder manipulation
+
+
+import os
+import zipfile
+
+#from shutil import copy
+#from shutil import rmtree
+
+def unZipBinaries():
+    return
+
+unZipBinaries()
 
 
 
