b05a06a562d0aed2d6269f88e3f30f6e86119172
Bastian Awischus <bastian.awischus@zii.aero>
2020-09-30 17:38:36 -0700

    added zip, able to unzip, need to unzip in the desired directory, need to be able to parse only thouhg list using the partial names.

--- main.py
+++ main.py
@@ -5,20 +5,33 @@
 
 import os
 import zipfile
-
+from pathlib import Path
 #from shutil import copy
 #from shutil import rmtree
 
 def unZipBinaries():
     #unzip all
     #delete zip files
-    return
-    
-def prepareFilesForDeploy():
-    return
+    zipList = ['cms.mediasuite.rim_52','RIM_CMS_v52','RIM_ContentSetPublish_v52']
+        # Need to be able to parse these with only containing mediasuite, CMS , ContentSetPublish
+
+    for name in zipList:
+        with zipfile.ZipFile("buildSourceBinaryFiles" + name,"r") as zip_ref: #The file path is not working with this.
+            zip_ref.extractall(os.getcwd())
+        # TODO 
+        #go into dir
+        #check if the zip exsit 
+        #yes unzip
+        #no go to next one
+
+
+
+
+#def prepareFilesForDeploy():
+    #return
 
 unZipBinaries()
-prepareFilesForDeploy()
+#prepareFilesForDeploy()
 
 
 
